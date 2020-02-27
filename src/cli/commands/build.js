c6aac0e00fcb327d5423e30d93ed10dc4966e5ec
谷山雄基 <cmt.0801nonki@gmail.com>
2020-02-27 20:58:17 +0900

    fix copy

--- src/cli/commands/build.js
+++ src/cli/commands/build.js
@@ -2,9 +2,10 @@ import path from 'path'
 import * as utils from '../utils'
 
 export function run() {
+  const outputDir = process.env.__OUTPUT_DIR__ || 'dist'
   utils.vueCliService('build').then(() => {
-    const source = path.resolve(__dirname, '../../../dist')
-    const destination = path.join(process.cwd(), 'dist')
+    const source = path.resolve(__dirname, `../../../${outputDir}`)
+    const destination = path.join(process.cwd(), outputDir)
     console.error(`${source},${destination}`)
     if (source === destination) return
     utils.copyFile(source, destination)
